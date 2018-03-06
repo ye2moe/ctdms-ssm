d85428dc37271a3987ff0768605d8d5b67e718d3
ye2moe <1059654342@qq.com>
2018-03-06 17:21:02 +0800

    init

---
+++ src/main/java/cn/jxufe/ctdms/CtdmsApplication.java
@@ -0,0 +1,13 @@
+package cn.jxufe.ctdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CtdmsApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/jxufe/ctdms/CtdmsApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.jxufe.ctdms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CtdmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
